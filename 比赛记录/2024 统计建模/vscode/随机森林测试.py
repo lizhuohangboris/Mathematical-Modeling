e0c67304611544fb11cdf04a49e8d82a674961a1
爱吃芒果的小孩 <112768268+lizhuohangboris@users.noreply.github.com>
2024-05-09 00:18:56 +0800

    更新

--- 比赛记录/2024 统计建模/vscode/随机森林测试.py
+++ 比赛记录/2024 统计建模/vscode/随机森林测试.py
@@ -1,3 +1,60 @@
+# import pandas as pd
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.model_selection import train_test_split
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# plt.rcParams['font.sans-serif'] = ['SimHei']  # 设置中文字体为黑体
+
+# # 读取数据
+# data = pd.read_excel("D:/Normal_tools/Github_desktop/Clone_shop/Mathematical-Modeling/比赛记录/2024 统计建模/随机森林.xlsx")
+
+# # 将"Month"列转换为日期类型
+# data['Month'] = pd.to_datetime(data['Month'], format='%b-%y')
+
+# # 将"Month"列拆分为年和月，并添加这两列作为特征
+# data['Year'] = data['Month'].dt.year
+# data['Month'] = data['Month'].dt.month
+
+# # 确定特征和目标列
+# X = data.drop(["AQI"], axis=1)
+# y = data["AQI"]
+
+# # 划分训练集和测试集
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# # 随机森林模型
+# model = RandomForestRegressor(n_estimators=150, min_samples_leaf=12, min_impurity_decrease=0.5, bootstrap=True, max_features=0.9, max_depth=3, random_state=42)
+
+# # 输出随机森林参数值
+
+
+# # 拟合模型
+# model.fit(X_train, y_train)
+
+# # 预测训练集和测试集的AQI值
+# y_train_pred = model.predict(X_train)
+# y_test_pred = model.predict(X_test)
+
+# # 计算模型在训练集和测试集上的R^2分数
+# train_score = model.score(X_train, y_train)
+# test_score = model.score(X_test, y_test)
+# print("Train R^2 Score:", train_score)
+# print("Test R^2 Score:", test_score)
+
+# # 绘制预测值与真实值的关系图
+# plt.scatter(y_train, y_train_pred, color='blue', label='Train')
+# plt.scatter(y_test, y_test_pred, color='red', label='Test')
+# plt.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+# plt.xlabel('真实AQI')
+# plt.ylabel('预测AQI')
+# plt.title('[ 随机森林 ] 真实AQI vs 预测AQI')
+# plt.legend()
+# plt.show()
+
+
+
+
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
@@ -40,7 +97,12 @@ train_score = model.score(X_train, y_train)
 test_score = model.score(X_test, y_test)
 print("Train R^2 Score:", train_score)
 print("Test R^2 Score:", test_score)
-
+print("estimator Maximum Number of Iterations:", model.n_estimators)
+print("min_child_sample Minimum Amount of Data on a Leaf Node:", model.min_samples_leaf)
+print("gamma Minimum Gain to be Added:", model.min_impurity_decrease)
+print("subsample Sample Percentage:", model.bootstrap)
+print("colsample Sample Proportion of the Variable:", model.max_features)
+print("max_depth Depth of the Tree:", model.max_depth)
 # 绘制预测值与真实值的关系图
 plt.scatter(y_train, y_train_pred, color='blue', label='Train')
 plt.scatter(y_test, y_test_pred, color='red', label='Test')
